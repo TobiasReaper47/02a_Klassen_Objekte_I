5ceecc5b226968baed773fe085076a02aca9f431
TobiasReaper47 <tobiasreaper273@gmail.com>
2025-01-16 10:44:07 +0100

    1. Iteration: Objekte durch new erzeugen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,10 @@ public class App {
     
     public static void main(String[] args) {
 
-
+        // 1. Iteration: Objekte durch new erzeugen
+        
+        System.out.println(new Cat());
+        System.out.println(new Cat());
     }
 
    
